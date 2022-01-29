4ee9cf05ab4c6655b6f253a55595e6753fedb7ba
11nithin <nithinjacobjames@hotmail.com>
2022-01-29 12:09:18 -0500

    style count

--- Realestate_query-2.sql
+++ Realestate_query-2.sql
@@ -8,7 +8,12 @@ INTO Stylecount
 FROM "REALESTATE"
 GROUP BY municipality, style;
 
+SELECT municipality, COUNT(style)
+INTO Stylecount
+FROM "REALESTATE"
+GROUP BY municipality;
 
+Select * from Stylecount;
 
 
 Select * from MunicipalityStyleCount;
